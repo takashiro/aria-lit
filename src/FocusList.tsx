26d57cea1e25e9e72b1e5701c1751ffcecc0c5fd
Kazuichi Takashiro <takashiro@qq.com>
2023-09-27 12:17:09 +0800

    Implement focus list

---
+++ src/FocusList.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import type BasicProps from './common/BasicProps';
+import findElements from './util/findElements';
+
+type Orientation = 'portrait' | 'landscape';
+
+interface FocusListProps<T> extends BasicProps, React.HTMLAttributes<T> {
+	/**
+	 * CSS selector to find interactive elements.
+	 */
+	selector: string;
+
+	/**
+	 * If a specific orientation is defined,
+	 *  - `portrait`: Only `ArrowUp` and `ArrowDown` will take effect.
+	 *  - `landscape`: Only `ArrowLeft` and `ArrowRight` will take effect.
+	 */
+	orientation?: Orientation;
+
+	/**
+	 * Keys to move focus to the next interactive element. (Default: `ArrowRight` and `ArrowDown`)
+	 */
+	forwardKeys?: string[];
+
+	/**
+	 * Keys to move focus to the previous interactive element. (Default: `ArrowLeft` and `ArrowUp`)
+	 */
+	backwardKeys?: string[];
+
+	/**
+	 * Whether focus should be moved in a circular order. (Default: `false`)
+	 */
+	circular?: boolean;
+}
+
+const calculateOffset = (key: string, forwardKeys: string[], backwardKeys: string[]): number => {
+	if (forwardKeys.includes(key)) {
+		return 1;
+	}
+	if (backwardKeys.includes(key)) {
+		return -1;
+	}
+	return 0;
+};
+
+const findNext = (
+	elements: HTMLElement[],
+	current: unknown,
+	offset: number,
+	circular?: boolean,
+): HTMLElement | undefined => {
+	const currentIndex = elements.findIndex(((child) => child === current));
+	let nextIndex = currentIndex + offset;
+	if (circular) {
+		if (nextIndex >= elements.length) {
+			nextIndex = 0;
+		} else if (nextIndex < 0) {
+			nextIndex = elements.length - 1;
+		}
+	}
+	return elements[nextIndex];
+};
+
+const getForwardKeys = (orientation?: Orientation): string[] => {
+	if (orientation === 'portrait') {
+		return ['ArrowDown'];
+	}
+	if (orientation === 'landscape') {
+		return ['ArrowRight'];
+	}
+	return ['ArrowRight', 'ArrowDown'];
+}
+
+const getBackwardKeys = (orientation?: Orientation): string[] => {
+	if (orientation === 'portrait') {
+		return ['ArrowUp'];
+	}
+	if (orientation === 'landscape') {
+		return ['ArrowLeft'];
+	}
+	return ['ArrowUp', 'ArrowLeft'];
+}
+
+export default function FocusList<T extends HTMLElement = HTMLDivElement>({
+	component: Component = 'div',
+	children,
+	onKeyDown,
+	selector,
+	orientation,
+	forwardKeys = getForwardKeys(orientation),
+	backwardKeys = getBackwardKeys(orientation),
+	circular,
+	...otherProps
+}: FocusListProps<T>): JSX.Element {
+	const handleKeyDown = (e: React.KeyboardEvent<T>): void => {
+		if (!e.ctrlKey && !e.altKey && !e.shiftKey && !e.metaKey) {
+			const offset = calculateOffset(e.key, forwardKeys, backwardKeys);
+			if (offset) {
+				const interactives = findElements(e.currentTarget, selector);
+				const next = findNext(interactives, e.target, offset, circular);
+				next?.focus();
+			}
+		}
+		onKeyDown?.(e);
+	};
+
+	return (
+		<Component onKeyDown={handleKeyDown} {...otherProps}>
+			{children}
+		</Component>
+	);
+}
---
+++ test/FocusList.spec.tsx
@@ -0,0 +1,171 @@
+import {
+	describe,
+	it,
+	expect,
+	beforeAll,
+	afterAll,
+} from '@jest/globals';
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react/pure';
+import userEvent from '@testing-library/user-event';
+
+import FocusList from '../src/FocusList';
+
+describe('Normal Focus List', () => {
+	let a: HTMLElement;
+	let b: HTMLElement;
+	let c: HTMLElement;
+
+	beforeAll(() => {
+		render(
+			<FocusList selector="button">
+				<button type="button" tabIndex={-1}>A</button>
+				<button type="button" tabIndex={-1}>B</button>
+				<button type="button" tabIndex={-1}>C</button>
+			</FocusList>,
+		);
+		a = screen.getByRole('button', { name: 'A' });
+		b = screen.getByRole('button', { name: 'B' });
+		c = screen.getByRole('button', { name: 'C' });
+	});
+
+	afterAll(() => {
+		cleanup();
+	});
+
+	it('can move focus forward', async () => {
+		a.focus();
+		await userEvent.keyboard('{ArrowRight}');
+		expect(document.activeElement).toBe(b);
+		await userEvent.keyboard('{ArrowDown}');
+		expect(document.activeElement).toBe(c);
+		await userEvent.keyboard('{ArrowDown}');
+		expect(document.activeElement).toBe(c);
+	});
+
+	it('can move focus backforward', async () => {
+		await userEvent.keyboard('{ArrowUp}');
+		expect(document.activeElement).toBe(b);
+		await userEvent.keyboard('{ArrowLeft}');
+		expect(document.activeElement).toBe(a);
+		await userEvent.keyboard('{ArrowLeft}');
+		expect(document.activeElement).toBe(a);
+	});
+
+	it('ignores other keys', async () => {
+		await userEvent.keyboard('{Enter}');
+		expect(document.activeElement).toBe(a);
+	});
+});
+
+describe('Circular Focus List', () => {
+	let a: HTMLElement;
+	let b: HTMLElement;
+	let c: HTMLElement;
+
+	beforeAll(() => {
+		render(
+			<FocusList selector="button" circular>
+				<button type="button" tabIndex={-1}>A</button>
+				<button type="button" tabIndex={-1}>B</button>
+				<button type="button" tabIndex={-1}>C</button>
+			</FocusList>,
+		);
+		a = screen.getByRole('button', { name: 'A' });
+		b = screen.getByRole('button', { name: 'B' });
+		c = screen.getByRole('button', { name: 'C' });
+	});
+
+	afterAll(() => {
+		cleanup();
+	});
+
+	it('can move focus forward', async () => {
+		a.focus();
+		await userEvent.keyboard('{ArrowRight}');
+		expect(document.activeElement).toBe(b);
+		await userEvent.keyboard('{ArrowDown}');
+		expect(document.activeElement).toBe(c);
+		await userEvent.keyboard('{ArrowDown}');
+		expect(document.activeElement).toBe(a);
+	});
+
+	it('can move focus backforward', async () => {
+		await userEvent.keyboard('{ArrowUp}');
+		expect(document.activeElement).toBe(c);
+		await userEvent.keyboard('{ArrowLeft}');
+		expect(document.activeElement).toBe(b);
+		await userEvent.keyboard('{ArrowLeft}');
+		expect(document.activeElement).toBe(a);
+	});
+});
+
+describe('Vertical Focus List', () => {
+	let a: HTMLElement;
+	let b: HTMLElement;
+
+	beforeAll(() => {
+		render(
+			<FocusList selector="button" orientation="portrait">
+				<button type="button" tabIndex={-1}>A</button>
+				<button type="button" tabIndex={-1}>B</button>
+			</FocusList>,
+		);
+		a = screen.getByRole('button', { name: 'A' });
+		b = screen.getByRole('button', { name: 'B' });
+	});
+
+	afterAll(() => {
+		cleanup();
+	});
+
+	it('accepts arrow down', async () => {
+		a.focus();
+		await userEvent.keyboard('{ArrowRight}');
+		expect(document.activeElement).toBe(a);
+		await userEvent.keyboard('{ArrowDown}');
+		expect(document.activeElement).toBe(b);
+	});
+
+	it('accepts arrow up', async () => {
+		await userEvent.keyboard('{ArrowLeft}');
+		expect(document.activeElement).toBe(b);
+		await userEvent.keyboard('{ArrowUp}');
+		expect(document.activeElement).toBe(a);
+	});
+});
+
+describe('Horizontal Focus List', () => {
+	let a: HTMLElement;
+	let b: HTMLElement;
+
+	beforeAll(() => {
+		render(
+			<FocusList selector="button" orientation="landscape">
+				<button type="button" tabIndex={-1}>A</button>
+				<button type="button" tabIndex={-1}>B</button>
+			</FocusList>,
+		);
+		a = screen.getByRole('button', { name: 'A' });
+		b = screen.getByRole('button', { name: 'B' });
+	});
+
+	afterAll(() => {
+		cleanup();
+	});
+
+	it('accepts arrow right', async () => {
+		a.focus();
+		await userEvent.keyboard('{ArrowDown}');
+		expect(document.activeElement).toBe(a);
+		await userEvent.keyboard('{ArrowRight}');
+		expect(document.activeElement).toBe(b);
+	});
+
+	it('accepts arrow left', async () => {
+		await userEvent.keyboard('{ArrowUp}');
+		expect(document.activeElement).toBe(b);
+		await userEvent.keyboard('{ArrowLeft}');
+		expect(document.activeElement).toBe(a);
+	});
+});
