507de5bc5318e9a777be0c96e0b4cd80be4935d3
Kazuichi Takashiro <takashiro@qq.com>
2023-10-08 23:58:37 +0800

    Implement toggle button

---
+++ src/ToggleButton.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import Clickable, { type GenericClickableProps } from './Clickable';
+import useChecked, { type ChangeEvent as CheckedChangeEvent } from './hooks/useChecked';
+
+type PressedState = React.HTMLAttributes<HTMLElement>['aria-pressed'];
+
+export interface ChangeEvent<T> extends React.ChangeEvent<T> {
+	/**
+	 * Whether the button is pressed.
+	 */
+	pressed: PressedState;
+}
+
+type RemovedProps = 'onTrigger' | 'defaultChecked' | 'checked';
+
+interface ToggleButtonProps<T> extends Omit<GenericClickableProps<T>, RemovedProps> {
+	defaultPressed?: PressedState;
+
+	pressed?: PressedState;
+
+	onChange?(e: ChangeEvent<T>): void;
+}
+
+/**
+ * [Toggle Button](https://www.w3.org/WAI/ARIA/apg/patterns/button/)
+ *
+ * A two-state button that can be either off (not pressed) or on (pressed).
+ */
+export default function ToggleButton<T extends HTMLElement = HTMLDivElement>({
+	defaultPressed,
+	pressed,
+	onChange,
+	...otherProps
+}: ToggleButtonProps<T>): JSX.Element {
+	const [value, toggle] = useChecked({
+		defaultChecked: defaultPressed,
+		checked: pressed,
+		onChange: onChange && (({ checked, ...e }: CheckedChangeEvent<T>) => onChange({
+			...e,
+			pressed: checked,
+		})),
+	});
+	return (
+		<Clickable<T>
+			role="button"
+			onTrigger={toggle}
+			aria-pressed={value}
+			{...otherProps}
+		/>
+	);
+}
--- test/ToggleButton.spec.tsx
+++ test/ToggleButton.spec.tsx
@@ -10,14 +10,15 @@ import {
 import React from 'react';
 import { cleanup, render, screen } from '@testing-library/react/pure';
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/jest-globals';
 
-import Switch, { ChangeEvent } from '../src/Switch';
+import ToggleButton, { ChangeEvent } from '../src/ToggleButton';
 
-describe('Switch (Uncontrolled)', () => {
+describe('Toggle Button (uncontrolled)', () => {
 	const onChange = jest.fn<(e: ChangeEvent<HTMLDivElement>) => void>();
 
 	beforeAll(() => {
-		render(<Switch onChange={onChange}>Click me!</Switch>);
+		render(<ToggleButton onChange={onChange}>Bold</ToggleButton>);
 	});
 
 	afterAll(() => {
@@ -29,50 +30,50 @@ describe('Switch (Uncontrolled)', () => {
 	});
 
 	it('can be triggered by mouse event', async () => {
-		await userEvent.click(screen.getByRole('switch', { name: 'Click me!', checked: false }));
+		await userEvent.click(screen.getByRole('button', { name: 'Bold', pressed: false }));
 		expect(onChange).toBeCalledTimes(1);
 		const e = onChange.mock.calls[0][0];
-		expect(e.checked).toBe(true);
-		const swt = screen.getByRole('switch', { name: 'Click me!', checked: true });
-		expect(document.activeElement).toBe(swt);
+		expect(e.pressed).toBe(true);
+		const button = screen.getByRole('button', { name: 'Bold', pressed: true });
+		expect(document.activeElement).toBe(button);
 	});
 
 	it('can be triggered by space key', async () => {
 		await userEvent.keyboard('{ }');
 		expect(onChange).toBeCalledTimes(1);
 		const [e] = onChange.mock.calls[0];
-		expect(e.checked).toBe(false);
+		expect(e.pressed).toBe(false);
 	});
 
 	it('can be triggered by enter key', async () => {
 		await userEvent.keyboard('{Enter}');
 		expect(onChange).toBeCalledTimes(1);
 		const [e] = onChange.mock.calls[0];
-		expect(e.checked).toBe(true);
+		expect(e.pressed).toBe(true);
 	});
 });
 
-describe('Switch (Controlled)', () => {
+describe('Other Toogle Buttons', () => {
 	const onChange = jest.fn<(e: ChangeEvent<HTMLDivElement>) => void>();
-
-	beforeAll(() => {
-		render(<Switch checked onChange={onChange}>Click me!</Switch>);
-	});
-
-	afterAll(() => {
-		cleanup();
-	});
 
 	afterEach(() => {
 		onChange.mockClear();
 	});
 
 	it('can be triggered by mouse event', async () => {
-		await userEvent.click(screen.getByRole('switch', { name: 'Click me!', checked: true }));
+		render(<ToggleButton pressed onChange={onChange}>Italic</ToggleButton>);
+		await userEvent.click(screen.getByRole('button', { name: 'Italic', pressed: true }));
 		expect(onChange).toBeCalledTimes(1);
 		const e = onChange.mock.calls[0][0];
-		expect(e.checked).toBe(false);
-		const swt = screen.getByRole('switch', { name: 'Click me!', checked: true });
-		expect(document.activeElement).toBe(swt);
+		expect(e.pressed).toBe(false);
+		const button = screen.getByRole('button', { name: 'Italic', pressed: true });
+		expect(document.activeElement).toBe(button);
+		cleanup();
+	});
+
+	it('can be pressed by default', async () => {
+		render(<ToggleButton defaultPressed onChange={onChange}>Italic</ToggleButton>);
+		const button = screen.getByRole('button', { name: 'Italic', pressed: true });
+		expect(button).toBeVisible();
 	});
 });
