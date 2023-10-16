90356db11b0c0e45f246da47e654b4dbb0995744
Kazuichi Takashiro <takashiro@qq.com>
2023-10-16 13:44:23 +0800

    Rewrite `ToggleButton` into web component

---
+++ src/ToggleButton.ts
@@ -0,0 +1,56 @@
+import Button from './Button';
+
+type PressedState = boolean | 'mixed';
+
+export interface ChangeEventInit {
+	/**
+	 * Whether the button is pressed.
+	 */
+	pressed: PressedState;
+}
+
+export type ChangeEvent = CustomEvent<ChangeEventInit>;
+
+export interface ToggleButtonProps {
+	/**
+	 * Whether the button is pressed.
+	 */
+	pressed?: PressedState;
+}
+
+/**
+ * [Toggle Button](https://www.w3.org/WAI/ARIA/apg/patterns/button/)
+ *
+ * A two-state button that can be either off (not pressed) or on (pressed).
+ */
+export class ToggleButton extends Button implements ToggleButtonProps {
+	pressed?: PressedState;
+
+	toggle(): void {
+		this.pressed = !this.pressed;
+		this.ariaPressed = String(this.pressed);
+		this.dispatchEvent(new CustomEvent<ChangeEventInit>('change', {
+			detail: {
+				pressed: this.pressed,
+			},
+			bubbles: true,
+			composed: true,
+		}));
+	}
+
+	override connectedCallback(): void {
+		super.connectedCallback();
+		this.addEventListener('trigger', this.#handleTrigger);
+	}
+
+	override disconnectedCallback(): void {
+		this.removeEventListener('trigger', this.#handleTrigger);
+		super.disconnectedCallback();
+	}
+
+	#handleTrigger = (): void => {
+		this.toggle();
+	}
+}
+
+export default ToggleButton;
---
+++ test/ToggleButton.stories.ts
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { userEvent, within } from '@storybook/testing-library';
+import { jest, expect } from '@storybook/jest';
+import { html } from 'lit/html.js';
+
+import { ToggleButton, type ToggleButtonProps, type ChangeEvent } from '../src/ToggleButton';
+
+customElements.define('karuta-toggle', ToggleButton);
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'karuta-toggle': ToggleButton;
+	}
+}
+
+const onChange = jest.fn<void, [ChangeEvent]>();
+
+const meta: Meta<ToggleButtonProps> = {
+	title: 'Controls/ToggleButton',
+	tags: ['autodocs'],
+	render: () => html`
+		<karuta-toggle @change=${onChange}>Bold</karuta-toggle>
+	`,
+};
+
+export default meta;
+
+type Story = StoryObj<ToggleButtonProps>;
+
+export const Enabled: Story = {
+	async play({ canvasElement, step }): Promise<void> {
+		const screen = within(canvasElement);
+
+		await step('trigger it by mouse event', async () => {
+			onChange.mockClear();
+			await userEvent.click(screen.getByRole('button', { name: 'Bold' }));
+			expect(onChange).toBeCalledTimes(1);
+			const e = onChange.mock.calls[0][0];
+			expect(e.detail.pressed).toBe(true);
+			const button = screen.getByRole('button', { name: 'Bold', pressed: true });
+			expect(document.activeElement).toBe(button);
+		});
+
+		await step('trigger it by space key', async () => {
+			onChange.mockClear();
+			await userEvent.keyboard('{ }');
+			expect(onChange).toBeCalledTimes(1);
+			const [e] = onChange.mock.calls[0];
+			expect(e.detail.pressed).toBe(false);
+		});
+
+		await step('trigger it by enter key', async () => {
+			onChange.mockClear();
+			await userEvent.keyboard('{Enter}');
+			expect(onChange).toBeCalledTimes(1);
+			const [e] = onChange.mock.calls[0];
+			expect(e.detail.pressed).toBe(true);
+		});
+	}
+};
